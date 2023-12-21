6b4dd1ceaba907cd630b91dcc15d5afd03d53c81
dhingra <chirag.dhingra@adesso.de>
2023-12-21 10:49:23 +0100

    making the square interactive

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
 
-export default function Board(){
-  return ( <>
+function Square() {
+  const [value, setValue] = useState<String>("-");
+
+  function handleClick() {
+    setValue('X')
+  }
+  return (
+  <button
+    className="square"
+    onClick={handleClick}
+  >
+    {value}
+  </button>)
+}
+
+export default function Board() {
+  return (<>
     <div className="board-row">
-      <Square value= "1"/>
-      <Square value= "2"/>
-      <Square value= "3"/>
+      <Square/>
+      <Square/>
+      <Square/>
     </div>
     <div className="board-row">
-      <Square value= "4"/>
-      <Square value= "5"/>
-      <Square value= "6"/>
+      <Square/>
+      <Square/>
+      <Square/>
     </div>
     <div className="board-row">
-      <Square value= "7"/>
-      <Square value= "8"/>
-      <Square value= "9"/>
+      <Square/>
+      <Square/>
+      <Square/>
     </div>
   </>);
 }
 
-function Square({value}:{value: String}){
-  return <button className="square">{value}</button>
-}
 
