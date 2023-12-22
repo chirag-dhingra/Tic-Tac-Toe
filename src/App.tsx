ad48ac7833e5f4dd72e97a72bf5b4d53acb4f030
dhingra <chirag.dhingra@adesso.de>
2023-12-22 13:07:45 +0100

    game finished

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,35 @@ import "./App.css";
 function App() {
   const [state, setState] = useState(Array(9).fill(null));
   const [currentTurn, setCurrentTurn] = useState("X");
-
+  
+  const checkWinner = (state: any[]) =>{
+    const win = [
+      [0,1,2],
+      [3,4,5],
+      [6,7,8],
+      [0,3,6],
+      [1,4,7],
+      [2,5,8],
+      [0,4,8],
+      [2,4,6],
+    ];
+    for( let i = 0; i < win.length; i++){
+      const [a,b,c] = win[i];
+      if(state[a] !== null && state[a] === state[b] && state[a] === state[c] )
+        return true;
+    }
+    return false;
+  }
   const handleBlockClick = (index: number) => {
     const stateCopy = Array.from(state);
     stateCopy[index] = currentTurn;//index will be given by a click and that index will be set to currentTurn(X or 0)
+    
+    //Check whether someone won
+     const win = checkWinner(stateCopy);
+
+     if(win){
+      alert(`${currentTurn} won the game`);
+     }
     setCurrentTurn(currentTurn === "X" ? "0" : "X"); //Flip the turn 
     setState(stateCopy)
   }
